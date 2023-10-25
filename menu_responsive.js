a4f28b400e7b578efc7179a387999f14fa058d7c
DaniG1menez <dani.gimenez5330@gmail.com>
2023-10-25 01:54:46 -0300

    dani

---
+++ menu_responsive.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
