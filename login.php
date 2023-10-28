c7c0bede87957aa4962f49e07e0996fe118666c8
Busferis <busferis7@gmail.com>
2023-10-27 22:50:33 -0300

    Actualizacion

--- login.php
+++ login.php
@@ -69,14 +69,14 @@ if (isset($_SESSION["registro_exitoso"])) {
     <link rel="icon" href="img/logo_pestaña.png">
 	<title></title>
 	<link rel="stylesheet" href="css/login.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
 	rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
-	crossorigin="anonymous">
+	crossorigin="anonymous"> -->
 </head>
 <body class="releway">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
 	integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
-	crossorigin="anonymous"></script>
+	crossorigin="anonymous"></script> -->
 	<div class="login">
 		<h1 class="roboto">Iniciar sesión</h1>
 	    <form action="login.php" method="POST">
