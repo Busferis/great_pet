0217608a9a857faf7ebb34edd6cd1cb89f2bf74a
DaniG1menez <dani.gimenez5330@gmail.com>
2023-11-02 03:30:26 -0300

    dsa

--- login.php
+++ login.php
@@ -69,6 +69,7 @@ if (isset($_SESSION["registro_exitoso"])) {
     <link rel="icon" href="img/logo_pestaña.png">
 	<title></title>
 	<link rel="stylesheet" href="css/login.css">
+	<!-- <link rel="stylesheet" href="css/color.css"> -->
 	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
 	rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
 	crossorigin="anonymous"> -->
@@ -77,37 +78,44 @@ if (isset($_SESSION["registro_exitoso"])) {
 	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
 	integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
 	crossorigin="anonymous"></script> -->
-	<div class="login">
-		<h1 class="roboto">Iniciar sesión</h1>
-	    <form action="login.php" method="POST">
-	        <div class="input">
-	            <input type="text" name="email" placeholder="Correo electronico" required>
-	            <input type="password" name="password" placeholder="Contraseña" id="myInput" required>
-	            <div>
-	                <input type="checkbox" onclick="myFunction()"><label>Mostrar contraseña</label>
-	            </div>
-	        </div>
-	        <div class="botones">
-	            <a href="registro.php">Registrarse</a>
-	            <input type="submit" value="Ingresar">
-	        </div>
-	        <script>
-	            function myFunction() {
-	            var x = document.getElementById("myInput");
-	            if (x.type === "password") {
-	                x.type = "text";
-	            } else {
-	                x.type = "password";
-	            }
-	            //   var y = document.getElementById("myInput2");
-	            //   if (y.type === "password") {
-	            //     y.type = "text";
-	            //   } else {
-	            //     y.type = "password";
-	            //   }
-	            }
-	        </script>
-	    </form>
-    </div>
+	<div class="caja_contenedora_login_img">
+
+		<div class="img-logo-gp"></div>
+
+		<div class="login">
+			<h1 class=" titulo_i_s lexend ">Iniciar sesión</h1>
+			<form action="login.php" method="POST" class="contenedor_inputs_mas">
+				<div class="input">
+					<input type="text" name="email" placeholder="Correo electronico" required>
+					<input type="password" name="password" placeholder="Contraseña" id="myInput" required>
+					<div class="contenedor_mostrar_contraseña">
+						<input type="checkbox" onclick="myFunction()" class="contenedor_mostrar_contraseña"><label class="mos_con_s">Mostrar contraseña</label>
+					</div>
+				</div>
+				<div class="botones">
+					<input type="submit" value="Ingresar" class="cajas_re_ingresar">
+					<a href="registro.php" class=" registrarse_style cajas_re_ingresar ">Registrarse</a>
+				</div>
+				<script>
+					function myFunction() {
+					var x = document.getElementById("myInput");
+					if (x.type === "password") {
+						x.type = "text";
+					} else {
+						x.type = "password";
+					}
+					//   var y = document.getElementById("myInput2");
+					//   if (y.type === "password") {
+					//     y.type = "text";
+					//   } else {
+					//     y.type = "password";
+					//   }
+					}
+				</script>
+			</form>
+		</div>
+
+
+	</div>
 </body>
 </html>
