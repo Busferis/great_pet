88b0a18a34091fd99372b13213c58c423b4c91b3
Busferis <busferis7@gmail.com>
2023-11-03 19:53:40 -0300

    Actualizacion

--- api/usuarioModel.php
+++ api/usuarioModel.php
@@ -120,6 +120,41 @@
 			return $list;
 		}
 
+		public function listByCantPageBusqueda($parameters){
+
+			define("CANT", 0);
+			define("PAGE", 1);
+
+			if(!isset($parameters[CANT])){
+				return array("errno" => 400, "error" => "Falta especificar la cantidad");
+			}
+			
+			$cant = $parameters[CANT];
+
+			if(!isset($parameters[PAGE])){
+				return array("errno" => 400, "error" => "Falta especificar la pagina");
+			}
+			
+			$page = ($parameters[PAGE]-1)*$cant;
+
+			$limit = " LIMIT $page, $cant";
+
+			$response = $this->db->query("SELECT * FROM mascotas WHERE estado='Perdido' $limit");
+
+			$list = array("errno" => 400, "error" => "No hay usuarios");
+
+			if($response->num_rows > 0 ){
+
+				// responde con una matriz asociativa
+				$list = $response->fetch_all(MYSQLI_ASSOC);
+
+				//$list = array_merge(array("errno" => 200, "error" => "Se han listado usuarios", "num_rows" => $response->num_rows), $list);
+
+			}
+
+			return $list;
+		}
+
 		public function eliminarMascota($id_mascota){
 
 			$id_valor = $id_mascota[0];
